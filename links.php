31dafe37d7bdb19b8548d11849cf322c59c13613
akhiln66 <30778439+akhiln66@users.noreply.github.com>
2019-02-13 16:39:29 +0530

    link_file

--- links.php
+++ links.php
@@ -1,19 +1,19 @@
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
         <!-- CSS Bootstrap & Custom -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-         <!--<link href="css/font-awesome.min.css" rel="stylesheet">-->
+        <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+         <!--<link href="/css/font-awesome.min.css" rel="stylesheet">-->
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
      	
-		<link href="css/animate.css" rel="stylesheet" media="screen">
+		<link href="/css/animate.css" rel="stylesheet" media="screen">
 		
-        <link href="css/main.css" rel="stylesheet" media="screen">
+        <link href="/css/main.css" rel="stylesheet" media="screen">
        
         <!-- Favicons -->
         
         <!-- JavaScripts -->
-        <script src="js/jquery-1.10.2.min.js"></script>
+        <script src="/js/jquery-1.10.2.min.js"></script>
 			
-        <script src="js/modernizr.min.js"></script>
+        <script src="/js/modernizr.min.js"></script>
         <script src="http://richhollis.github.io/vticker/downloads/jquery.vticker.min.js?v=1.15"></script>
         <!--[if lt IE 8]>
 	    <div style=' clear: both; text-align:center; position: relative;'>
